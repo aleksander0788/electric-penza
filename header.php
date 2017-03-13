13f71712f6747290cc986547f6601cb67a74b768
newyorrker <newyorrker@gmail.com>
2017-03-13 20:30:03 +0300

    Bootstrap & JS

---
+++ header.php
@@ -0,0 +1,21 @@
+<header>
+	<a name="replacement"></a>
+		<div class="container">
+			<div class="col-xs-2"><a href="#"><img src="images/logo-top.png" alt="" class="logo-top"></a></div>
+			<div class="col-xs-7">
+				<nav class="menu-top">
+					<ul>
+						<li class="navbar"><a href="#services">Услуги</a></li>
+						<li class="navbar"><a href="#prices">Цены</a></li>
+						<li class="navbar"><a href="#advantages">Преимущества</a></li>
+						<li class="navbar"><a href="#call-master">Вызов мастера	</a></li>
+						<li class="navbar"><a href="#contacts">Контакты</a></li>
+					</ul>
+				</nav>
+			</div>
+			<div class="col-xs-3 phone-number">
+				<a href="tel:+78412395068">+7 (8412) 39-26-02</a>
+				<p>Вызвать электрика</p>
+			</div>
+		</div>
+	</header>
