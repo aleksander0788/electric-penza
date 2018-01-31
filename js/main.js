1affefd82bc637f2754a5a01d54c603d1fa66ee6
newyorrker <newyorrker@gmail.com>
2018-01-31 17:19:12 +0300

    fix

--- js/main.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($) {
-	$('.carousel').slick({
-		autoplay: true,
-		autoplaySpeed: 7000,
-		nextArrow: '<button class="slick-arrow slick-next">next</button>',
-		prevArrow: '<button class="slick-arrow  slick-prev">prev</button>',
-		arrows: false,
-	});
-});
